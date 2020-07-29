dec2d610aaa94085d6ebcf8aba2c5384ed4e9709
Arsiema berhane <Arsiema.ab@gmail.com>
2020-07-28 23:43:47 -0400

    first commit

---
+++ NewLaptopSetup.java
@@ -0,0 +1,14 @@
+package FirstFolder;
+
+public class NewLaptopSetup {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Good Bye World");
+        System.out.println("Greetings World");
+        System.out.println("Good night world");
+
+
+
+    }
+
+}
